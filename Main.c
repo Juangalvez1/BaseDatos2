0a9f98ca286f346a77f3a0172d170b8f110581c6
birulboy <brahian.castro@utp.edu.co>
2024-11-18 23:08:40 -0500

    actu

--- Main.c
+++ Main.c
@@ -22,7 +22,8 @@
 #include <string.h>
 #include <time.h>
 #include "Functions.c"
-#include "ExecuteOption2.c"
+#include "ExecuteOption2Arrays.c"
+//#include "ExecuteOption2Files.c"
 #include "ExecuteOption5.c"
 
 
